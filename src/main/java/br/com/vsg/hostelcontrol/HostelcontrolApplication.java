c1445e83ceb05034b4eb53a6e3b1571648b97f20
Stênio Galvão <steniogalvao@gmail.com>
2018-12-15 23:02:25 +0200

    First commit
    first commit

---
+++ src/main/java/br/com/vsg/hostelcontrol/HostelcontrolApplication.java
@@ -0,0 +1,14 @@
+package br.com.vsg.hostelcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelcontrolApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/br/com/vsg/hostelcontrol/ServletInitializer.java
@@ -0,0 +1,14 @@
+package br.com.vsg.hostelcontrol;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HostelcontrolApplication.class);
+	}
+
+}
+
